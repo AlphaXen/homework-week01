c03279275dfb246f393a454d8e5dfbc49b3c0d30
AlphaXen <kimminjae771@naver.com>
2025-09-07 02:40:15 +0900

    feat: Add else block to check for remainder 2 in if statement

--- lib/week01/1.6/1_if_keyword.dart
+++ lib/week01/1.6/1_if_keyword.dart
@@ -3,8 +3,9 @@ void main() {
 
   if (number % 3 == 0) {
     print('3의 배수입니다');
-  } else if (number % 3 ==1) {
+  } else if (number % 3 == 1) {
     print('나머지가 1입니다.');
-  }
+  } else {
+    print('나머지가 2입니다.');
   }
 }
