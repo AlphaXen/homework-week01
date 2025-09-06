d7083aa92b94afc4c350c6ba49ecad171d0f1e60
AlphaXen <kimminjae771@naver.com>
2025-09-07 02:42:15 +0900

    feat: Add switch keyword example

---
+++ lib/week01/1.6/2_switch_keyword.dart
@@ -0,0 +1,17 @@
+enum Status { approved, pending, rejected }
+
+void main() {
+  Status status = Status.approved;
+
+  switch (status) {
+    case Status.approved:
+      print('승인됨');
+      break;
+    case Status.pending:
+      print('대기중');
+      break;
+    case Status.rejected:
+      print('거절됨');
+      break;
+  }
+}
