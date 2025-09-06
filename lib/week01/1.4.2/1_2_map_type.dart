4a148afa0cfaab6a91eb9cba085bb7d9135a1272
AlphaXen <kimminjae771@naver.com>
2025-09-07 02:04:45 +0900

    feat: Print Map keys and values in 1_2_map_type.dart

--- lib/week01/1.4.2/1_2_map_type.dart
+++ lib/week01/1.4.2/1_2_map_type.dart
@@ -4,4 +4,8 @@ void main() {
     'Ron Weasley': '론 위즐리',
     'Hermione Granger': '헤르미온느 그레인저',
   };
+
+  print(dictionary.keys);
+  // Iterable이 반환되기 때문에 . toList()를 실행하여 List를 반환받을 수도 있음
+  print(dictionary.values);
 }
