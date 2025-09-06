7732165e81db824ccc83263de6c7bd762395c89d
AlphaXen <kimminjae771@naver.com>
2025-09-07 02:09:15 +0900

    feat: Add basic set usage examples to 1_set_type.dart

--- lib/week01/1.4.3/1_set_type.dart
+++ lib/week01/1.4.3/1_set_type.dart
@@ -1,3 +1,12 @@
 void main() {
-  Set<String> blackPink = {'로제', '지수', '리사', '제니', '제니'};
+  Set<String> blackPink = {'로제', '지수', '리사', '제니', '제니'}; // 제니 중복
+
+  print(blackPink);
+  print(blackPink.contains('로제')); // 값이 있는지 확인하기
+  print(blackPink.toList()); // 리스트로 변환하기
+
+  List<String> blackPink2 = ['로제', '지수', '지수']
+
+
+
 }
