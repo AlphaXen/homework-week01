5a90cbc8dc25ae0ae7b3ae26998d32755b7499bf
AlphaXen <kimminjae771@naver.com>
2025-09-07 01:45:44 +0900

    Fix: Convert Iterable to List in where function example

--- lib/week01/1.4/3_where_function.dart
+++ lib/week01/1.4/3_where_function.dart
@@ -1,11 +1,10 @@
-void main(){
+void main() {
   List<String> blackPinkList = ['리사', '지수', '제니', '로제'];
-  
-  final neList = blackPinkList.where(
+
+  final newList = blackPinkList.where(
     (name) => name == '리사' || name == '지수', //'리사' 또는 '지수'만 유지
   );
 
   print(newList);
-  
-  ,)
+  print(newList.toList()); // Iterable을 List로 다시 변환할 때 .toList() 사용
 }
