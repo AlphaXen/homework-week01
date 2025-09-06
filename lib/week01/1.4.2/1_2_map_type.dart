6905acaafdbaf85e071ba5cb9a95fd8508432afe
AlphaXen <kimminjae771@naver.com>
2025-09-07 02:00:45 +0900

    Fix: Corrected map initialization in 1_2_map_type.dart

--- lib/week01/1.4.2/1_2_map_type.dart
+++ lib/week01/1.4.2/1_2_map_type.dart
@@ -2,6 +2,6 @@ void main() {
   Map<String, String> dictionary = {
     'Harry Potter': '해리 포터',
     'Ron Weasley': '론 위즐리',
-    'Hermione Granger',
+    'Hermione Granger': '헤르미온느 그레인저',
   };
 }
