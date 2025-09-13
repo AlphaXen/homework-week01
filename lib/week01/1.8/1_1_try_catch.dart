23048b1dd5c194e9047b60674a8d56bd0afbee47
AlphaXen <kimminjae771@naver.com>
2025-09-14 02:35:37 +0900

    Fix: Add try-catch block for error handling

--- lib/week01/1.8/1_1_try_catch.dart
+++ lib/week01/1.8/1_1_try_catch.dart
@@ -1,10 +1,11 @@
 void main() {
-  try{
-
-
+  try {
     // 에러가 없을 때 실행할 로직
     final String name = '코드팩토리';
-    
-    print(name);
-      }
+
+    print(name); //에러가 없으니 출력됨
+  } catch (e) {
+    // 에러가 있을 때 실행할 로직
+    print(e);
+  }
 }
