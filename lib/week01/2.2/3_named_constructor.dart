6221fec55f18b3cdaa6ddc736b66e33e2b73aef7
AlphaXen <kimminjae771@naver.com>
2025-09-14 02:57:08 +0900

    Fix: Pass membersCount to Idol.fromMap constructor

--- lib/week01/2.2/3_named_constructor.dart
+++ lib/week01/2.2/3_named_constructor.dart
@@ -1,12 +1,9 @@
-import 'package:myapp/week01/2.2/2_constructor.dart';
-
 void main() {
   // 기본 생성자 사용
   Idol blackPink = Idol('블랙핑크', 4);
   blackPink.sayName();
 
   // fromMap이라는 네임드 생성자 사용
-  Idol bts = Idol.fromMap({
-    'name': 'BTS', 
-  })
+  Idol bts = Idol.fromMap({'name': 'BTS', 'membersCount': 7});
+  bts.sayName();
 }
