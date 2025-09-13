1206c91c4048edacec8359dde5af96778168141a
AlphaXen <kimminjae771@naver.com>
2025-09-14 02:58:08 +0900

    feat: Implement Idol class with a named constructor

--- lib/week01/2.2/3_named_constructor.dart
+++ lib/week01/2.2/3_named_constructor.dart
@@ -1,3 +1,11 @@
+class Idol {
+  final String name;
+  final int membersCount;
+
+  // 생성자
+  Idol(String name, int membersCount)
+}
+
 void main() {
   // 기본 생성자 사용
   Idol blackPink = Idol('블랙핑크', 4);
