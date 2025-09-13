4c69c33ab823f53d3b128aefaaed91ae86511420
AlphaXen <kimminjae771@naver.com>
2025-09-14 03:08:08 +0900

    Fix: Initialize membersCount in Idol constructor.

--- lib/week01/2.2/3_named_constructor.dart
+++ lib/week01/2.2/3_named_constructor.dart
@@ -3,7 +3,7 @@ class Idol {
   final int membersCount;
 
   // 생성자
-  Idol(String name, int membersCount)
+  Idol(String name, int membersCount, this.membersCount)
   // 1개 이상의 변수를 저장하고 싶을 때는 , 기호로 연결해주면 됩니다.
   : this.name = name,
     this.membersCount;
