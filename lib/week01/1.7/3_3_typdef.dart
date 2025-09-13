88e598f63fbe17c9b539076685a0d360fd9f43e8
AlphaXen <kimminjae771@naver.com>
2025-09-14 02:33:07 +0900

    feat: Implement typedef example with Operation function type

---
+++ lib/week01/1.7/3_3_typdef.dart
@@ -0,0 +1,15 @@
+import 'package:myapp/week01/1.7/3_2_typedef.dart';
+
+typedef Operation = void Function(int x, int y);
+
+void add(int x, int y) {
+  print('결괏값 : ${x + y}');
+}
+
+void calculate(int x, int y, Operation oper) {
+  oper(x, y);
+}
+
+void main() {
+  calculate(1, 2, add);
+}
