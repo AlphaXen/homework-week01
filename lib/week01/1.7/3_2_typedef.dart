616dfa3274149670964f237b117a3cca28a5ffeb
AlphaXen <kimminjae771@naver.com>
2025-09-14 02:28:37 +0900

    feat: Add `subtract` function using typedef `PathOperation`

--- lib/week01/1.7/3_2_typedef.dart
+++ lib/week01/1.7/3_2_typedef.dart
@@ -2,6 +2,10 @@ import 'package:flutter/cupertino.dart';
 
 typedef PathOperation = void Function(int x, int y);
 
-void add(int x, int y {
+void add(int x, int y) {
   print('결괏값 : ${x + y}');
+}
+
+void subtract(int x, int y) {
+  print('결괏값 : ${x - y}');
 }
