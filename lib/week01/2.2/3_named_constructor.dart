21798dad37507e75e1ba5111b7ff6de8313046de
AlphaXen <kimminjae771@naver.com>
2025-09-14 02:56:38 +0900

    feat: Implement named constructor example

--- lib/week01/2.2/3_named_constructor.dart
+++ lib/week01/2.2/3_named_constructor.dart
@@ -2,5 +2,11 @@ import 'package:myapp/week01/2.2/2_constructor.dart';
 
 void main() {
   // 기본 생성자 사용
-  Idol
+  Idol blackPink = Idol('블랙핑크', 4);
+  blackPink.sayName();
+
+  // fromMap이라는 네임드 생성자 사용
+  Idol bts = Idol.fromMap({
+    'name': 'BTS', 
+  })
 }
