c47d644feae44f7e846ac5e29b5dd9c5347c2f9a
AlphaXen <kimminjae771@naver.com>
2025-09-14 23:17:46 +0900

    Fix: Implement BoyGroup constructor and add sayMale method

--- lib/week02/2.3/1_inheritance.dart
+++ lib/week02/2.3/1_inheritance.dart
@@ -17,5 +17,10 @@ class Idol {
 // class 자식 클래스 extends 부모 클래스 순서입니다.
 class BoyGroup extends Idol {
     String name, int membersCount,) : super( //super는 부모 클래스를 지칭합니다.
-    )
+    name, membersCount,);
+
+    // 상속받지 않은 기능
+    void sayMale() {
+        print('저는 남자 아이돌입니다.');
+    }
 }
