f9ed39052e9544de7cfccef8b3b20a29db2f2bec
AlphaXen <kimminjae771@naver.com>
2025-09-15 00:31:17 +0900

    Fix: Correctly print result2 in returning_value.dart

--- lib/week02/3.3/2_returning_value.dart
+++ lib/week02/3.3/2_returning_value.dart
@@ -4,5 +4,5 @@ void main() async {
   fianl result = await addNumbers(1, 1);
   print('결괏값 $result'); // 일반 함수와 동일하게 반환값을 받을 수 있음
   final result2 = await addNumbers(2, 2);
-  print('결괏값 $result2');
+  print('결괏값 $result');
 }
