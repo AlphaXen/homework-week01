57612cb4f5eaefff2c19c8485b5298636b45c78e
AlphaXen <kimminjae771@naver.com>
2025-09-15 00:11:47 +0900

    feat: Implement basic addNumbers function with print statement

--- lib/week02/3.2/1_2_future.dart
+++ lib/week02/3.2/1_2_future.dart
@@ -1,3 +1,9 @@
 void main() {
-  addNumber
+  addNumbers(1, 1);
+}
+
+void addNumbers(int number1, int number2) {
+  print('$number1 + $number2 계산 시작!');
+
+  // future.delayed()를 사용하면 일정 시간 후에 콜백 함수를
 }
