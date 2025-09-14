58078d889eaff7162466bfed0318d10410c8ccbf
AlphaXen <kimminjae771@naver.com>
2025-09-15 00:14:17 +0900

    feat: Add Future.delayed to simulate async calculation

--- lib/week02/3.2/1_2_future.dart
+++ lib/week02/3.2/1_2_future.dart
@@ -6,4 +6,9 @@ void addNumbers(int number1, int number2) {
   print('$number1 + $number2 계산 시작!');
 
   // future.delayed()를 사용하면 일정 시간 후에 콜백 함수를 실행 할 수 있음
+  Future.delayed(Duration(seconds: 3), () {
+    print('$number1 + $number2 = ${number1 + number2}');
+  });
+
+  print('$number1 + $number2 코드 실행 끝');
 }
