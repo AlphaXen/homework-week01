ca7c85d2f4d5671e666c33c14f8713880c5455ec
AlphaXen <kimminjae771@naver.com>
2025-09-14 23:18:46 +0900

    Fix: BoyGroup constructor syntax and add missing comma

--- lib/week02/2.3/1_inheritance.dart
+++ lib/week02/2.3/1_inheritance.dart
@@ -16,11 +16,16 @@ class Idol {
 // extends 키워드를 이용하여 상속받습니다.
 // class 자식 클래스 extends 부모 클래스 순서입니다.
 class BoyGroup extends Idol {
-    String name, int membersCount,) : super( //super는 부모 클래스를 지칭합니다.
-    name, membersCount,);
+  //상속받은 생성자
+  BoyGroup(String name, int membersCount)
+    : super(
+        //super는 부모 클래스를 지칭합니다.
+        name,
+        membersCount,
+      );
 
-    // 상속받지 않은 기능
-    void sayMale() {
-        print('저는 남자 아이돌입니다.');
-    }
+  // 상속받지 않은 기능
+  void sayMale() {
+    print('저는 남자 아이돌입니다.');
+  }
 }
