0b063ec149b852144ea58b5f0b3ac7472487bf87
AlphaXen <kimminjae771@naver.com>
2025-09-15 00:12:17 +0900

    Fix: Explain future.delayed() functionality in 1_2_future.dart

--- lib/week02/3.2/1_2_future.dart
+++ lib/week02/3.2/1_2_future.dart
@@ -5,5 +5,5 @@ void main() {
 void addNumbers(int number1, int number2) {
   print('$number1 + $number2 계산 시작!');
 
-  // future.delayed()를 사용하면 일정 시간 후에 콜백 함수를
+  // future.delayed()를 사용하면 일정 시간 후에 콜백 함수를 실행 할 수 있음
 }
