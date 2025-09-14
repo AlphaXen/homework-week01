d3fb783156cb7856b5b3387f02f7c00d47ce70c7
AlphaXen <kimminjae771@naver.com>
2025-09-15 00:54:51 +0900

    Fix: Correct map literal syntax in 3.dart

--- lib/week02/4.2/3.dart
+++ lib/week02/4.2/3.dart
@@ -1,5 +1,5 @@
 void main() {
-  final minjiMap = {'name', '민지', 'age': 19};
+  final minjiMap = {'name': '민지', 'age': 19};
   // 위에 맵의 구조와 똑같은 구조로 구조 분해하면 됩니다.
   final {'name': name, 'age': age} = minjiMap;
 
