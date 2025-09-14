e761cbc3995351f0ef1dfc5fc60dc98af9a66776
AlphaXen <kimminjae771@naver.com>
2025-09-15 01:00:51 +0900

    feat: Implement basic Korean to English day conversion

--- lib/week02/4.3/1.dart
+++ lib/week02/4.3/1.dart
@@ -5,5 +5,11 @@ void main() {
   string dayEnglish = switch (dayKor) {
     // '=>'를 사용하면 switch문 조건에 맞을 때 값을 반환할 수 있습니다.
     '월요일' => 'Monday',
+    '화요일' => 'Tuesday',
+    '수요일' => 'Wedsday',
+    '목요일' => 'Monday',
+    '금요일' => 'Monday',
+    '토요일' => 'Monday',
+    '일요일' => 'Monday',
   }
 }
