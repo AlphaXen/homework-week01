ece0001258a7ec1b3160b48d87e92d76c6a6b35e
AlphaXen <kimminjae771@naver.com>
2025-09-15 00:31:47 +0900

    Fix: Corrected variable name in print statement for async function return value.

--- lib/week02/3.3/2_returning_value.dart
+++ lib/week02/3.3/2_returning_value.dart
@@ -1,8 +1,6 @@
-import 'package:myapp/week02/3.3/1_1_async_and_await.dart';
-
 void main() async {
   fianl result = await addNumbers(1, 1);
   print('결괏값 $result'); // 일반 함수와 동일하게 반환값을 받을 수 있음
   final result2 = await addNumbers(2, 2);
-  print('결괏값 $result');
+  print('결괏값 $result2');
 }
