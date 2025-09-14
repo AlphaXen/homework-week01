f0e486d0076ab36fbc182ce3be352fe38c661e40
AlphaXen <kimminjae771@naver.com>
2025-09-14 23:40:17 +0900

    feat(mixin): Demonstrate IdolSingMixin usage with BoyGroup

--- lib/week02/2.6/1_mixin.dart
+++ lib/week02/2.6/1_mixin.dart
@@ -12,3 +12,10 @@ class BoyGroup extends Idol with IdolSingMixin {
     print('저는 남자 아이돌입니다');
   }
 }
+
+void main() {
+  BoyGroup bts = BoyGroup('BTS', 7);
+
+  // 믹스인에 정의된 sing() 함수 사용 가능)
+  bts.sing();
+}
