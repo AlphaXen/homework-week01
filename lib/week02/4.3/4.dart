c2c7f01498d5525a2d9e27bd60eb236e9ab6c4d4
AlphaXen <kimminjae771@naver.com>
2025-09-15 01:10:21 +0900

    Fix: Correct typo and add a more complex expression using 'switch'.

--- lib/week02/4.3/4.dart
+++ lib/week02/4.3/4.dart
@@ -1,3 +1,10 @@
 void main() {
-  (int )
+  (int a, int b) val = (1, -1);
+
+  //default가 출력됩니다. 만약에 b값을 0 이상으로 변경하면
+  // 1, _를 출력할 수 있습니다.
+  switch (val) {
+    case (1, _) when val.$2 > 0:
+      print('1, _');
+  }
 }
