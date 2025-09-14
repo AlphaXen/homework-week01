91e50bda15cdf358f6a8da4b164249c64a1c830c
AlphaXen <kimminjae771@naver.com>
2025-09-15 00:35:17 +0900

    feat(week02): Return the sum from `addNumbers` function

--- lib/week02/3.3/2_returning_value.dart
+++ lib/week02/3.3/2_returning_value.dart
@@ -8,7 +8,11 @@ void main() async {
 Future<int> addNumbers(int number1, int number2) async {
   print('$number1 + $number2 계산 시작!');
 
-  await Future.delayed(Duration(seconds: 3), (){
-    print('$number1 + $number2 = ${number1 + $number2}');
-  })
+  await Future.delayed(Duration(seconds: 3), () {
+    print('$number1 + $number2 = ${number1 + number2}');
+  });
+
+  print('$number1 + $number2 코드 실행 끝');
+
+  return number1 + number2;
 }
