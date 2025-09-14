39ca15ca0be920ccdc87235184e242c461d9bb80
AlphaXen <kimminjae771@naver.com>
2025-09-15 00:06:17 +0900

    feat: Add comments to Future variable declarations in 1_1_future.dart

--- lib/week02/3.2/1_1_future.dart
+++ lib/week02/3.2/1_1_future.dart
@@ -1,3 +1,3 @@
-Future<String> name;
-Future<int> number;
-Future<String> isOpend;
+Future<String> name; // 미래에 받을 String 값
+Future<int> number; // 미래에 받을 int 값
+Future<bool> isOpend; // 미래에 받을 bool 값
