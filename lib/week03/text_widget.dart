1a900e74bf0950f2078516e52506935dcd1b2b38
AlphaXen <kimminjae771@naver.com>
2025-09-19 23:43:47 +0900

    feat: Style text widget with custom font, weight, and color

--- lib/week03/text_widget.dart
+++ lib/week03/text_widget.dart
@@ -1,5 +1,15 @@
+import 'dart:ui';
+
 Text(
   // 작성하고 싶은 글
   '코드팩토리',
-  // 글자에 
+  // 글자에 스타일 적용
+  style: TextStyle(
+    // 글자 크기
+    fontSize: 16
+    // 글자 굴기
+    fontWeight: FontWeight.w700,
+    // 글자 색상
+    color: Colors.blue,
+  ),
 )
