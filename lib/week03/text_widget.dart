9870cba4f066f9d97dac6aaf296c91778fea1956
AlphaXen <kimminjae771@naver.com>
2025-09-19 23:44:47 +0900

    feat: Remove placeholder text widget.

--- lib/week03/text_widget.dart
+++ lib/week03/text_widget.dart
@@ -8,19 +8,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(
-  // 작성하고 싶은 글
-  '코드팩토리',
-  // 글자에 스타일 적용
-  style: TextStyle(
-    // 글자 크기
-    fontSize: 16
-    // 글자 굴기
-    fontWeight: FontWeight.w700,
-    // 글자 색상
-    color: Colors.blue,
-  ),
-)
+                    
         ),
       ),
     );
