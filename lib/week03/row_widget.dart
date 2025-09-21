dee2731ea9537dc5a339ad81d7ff3a875cbd63f6
AlphaXen <kimminjae771@naver.com>
2025-09-22 00:13:01 +0900

    feat(row_widget): Implement basic Row widget example

--- lib/week03/row_widget.dart
+++ lib/week03/row_widget.dart
@@ -5,5 +5,20 @@ void main() {
 }
 
 class RowWidgetExample extends StatelessWidget {
-  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SizedBox(
+          // 반대축에서 이동할 공간을 제공하기 위해 높이를 최대한으로 설정
+          height: double.infinity,
+          child: Row(
+
+            // 주축 정렬 지정
+            mainAxisAligment: MainAxisAlignment
+          ),
+        )
+      )
+    )
+  }
 }
