a9c1b3be7b1e5a669935064fc9b173321c130c96
AlphaXen <kimminjae771@naver.com>
2025-09-21 23:55:30 +0900

    feat: Add red Container to SizedBox for visual debugging

--- lib/week03/sized_box_widget.dart
+++ lib/week03/sized_box_widget.dart
@@ -19,8 +19,12 @@ class MyApp extends StatelessWidget {
             // 너비 지정
             width: 200.0,
 
-            // SizedBox는 색사이 
-          )
+            // SizedBox는 색상이 없으므로 크기를 확인하는
+            // 용도로 Container 추가
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
         ),
       ),
     );
