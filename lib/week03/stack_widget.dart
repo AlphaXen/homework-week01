65447878eddae26bdfba9ee0191bee0cd13f135d
AlphaXen <kimminjae771@naver.com>
2025-09-22 00:31:31 +0900

    feat: Implement basic Stack widget example

---
+++ lib/week03/stack_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Stack(
+            children: [
+              // 빨간색 Container
+              Container(
+                height: 300.0,
+                width: 300.0,
+                color: Colors.red,
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
