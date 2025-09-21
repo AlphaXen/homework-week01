4932d9b998d401503174f3bb0dbee5de58715cfe
AlphaXen <kimminjae771@naver.com>
2025-09-22 00:08:00 +0900

    feat: Add margin to blue container in PaddingMarginWidget

--- lib/week03/padding_margin_widget.dart
+++ lib/week03/padding_margin_widget.dart
@@ -12,9 +12,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          // 최상위 컨테이너
+          // 최상위 검정 컨테이너 (margin이 적용되는 대상)
           child: Container(
             color: Colors.black,
+
+            // 중간 파란 컨테이너
+            child: Container(
+              color: Colors.blue,
+
+              // 마진 적용 위치
+              margin: EdgeInset.all(16.0),
+            ),
           )
         ),
       ),
