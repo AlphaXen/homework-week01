cec3bc013b9338995dcfb8832931f527eadb8038
AlphaXen <kimminjae771@naver.com>
2025-09-22 00:30:01 +0900

    feat: Add Expanded widget example

---
+++ lib/week03/expanded_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              // 파란색 Container
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                ),
+              ),
+
+              // 빨간색 Container
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
