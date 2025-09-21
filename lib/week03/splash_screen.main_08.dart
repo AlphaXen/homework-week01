624ef79b0ef86d34d3510443a987ee00e0f63915
AlphaXen <kimminjae771@naver.com>
2025-09-22 00:47:31 +0900

    feat: Customize splash screen progress indicator color

--- lib/week03/splash_screen.main_08.dart
+++ lib/week03/splash_screen.main_08.dart
@@ -24,7 +24,11 @@ class SplashScreen extends StatelessWidget {
                     'assets.logo.png',
                     width: 200,
                   ),
-                  CircularProgressIndicator(),
+                  CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(
+                      Colors.white,
+                    ),
+                  ),
                 ],
               ),
             ],
