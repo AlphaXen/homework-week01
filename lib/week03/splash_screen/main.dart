9d06393ff069ca182877a759fb95fd3e417bb62e
AlphaXen <kimminjae771@naver.com>
2025-09-22 09:36:37 +0900

    Fix: Correct logo asset path in SplashScreen

--- lib/week03/splash_screen/main.dart
+++ lib/week03/splash_screen/main.dart
@@ -23,7 +23,7 @@ class SplashScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Image.asset(
-                    'assets.logo.png',
+                    'assets/logo.png',
                     width: 200,
                   ),
                   CircularProgressIndicator(
