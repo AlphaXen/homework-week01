0bbeacc436e7ca13ee0a592322194bc19c701585
AlphaXen <kimminjae771@naver.com>
2025-09-28 01:54:23 +0900

    feat(home_screen): Add basic app bar and body

--- lib/week03/blog_web_app/screen/home_screen.dart
+++ lib/week03/blog_web_app/screen/home_screen.dart
@@ -13,11 +13,18 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       // 앱바 위젯 추가
       appBar: AppBar(
+
         // 배경색 지정
         backgroundColor: Colors.orange,
 
+        // 앱 타이틀 지정
+        title: Text('Code Factory'),
+
+        // 가운데 정렬
+        centerTitle: true,
       ),
-    )
+      body: Text('Home Screen'),
+    );
   }
 }
 
