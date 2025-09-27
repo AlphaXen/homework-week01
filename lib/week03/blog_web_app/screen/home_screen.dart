973792910cecc442953dcebd3d8fe44423377ef1
AlphaXen <kimminjae771@naver.com>
2025-09-28 01:57:54 +0900

    feat: Load Code Factory blog in WebView on Home Screen

--- lib/week03/blog_web_app/screen/home_screen.dart
+++ lib/week03/blog_web_app/screen/home_screen.dart
@@ -4,7 +4,13 @@ import 'package:webview_flutter/webview_flutter.dart';
 class HomeScreen extends StatelessWidget {
 
   // WebViewController 선언
-  WebViewController webViewController = WebViewController();
+  WebViewController webViewController = WebViewController()
+
+  // WebViewController의 loadRequest() 함수를 실행합니다.
+  ..loadRequest(Uri.parse('https://blog.codefactory.ai'))
+
+  // JavaScript가 제한 없이 실행될 수 있도록 합니다.
+  ..setJavaScriptMode(JavaScriptMode.ursetricted)
 
   HomeScreen({Key? key}) : super(key: key);
 
