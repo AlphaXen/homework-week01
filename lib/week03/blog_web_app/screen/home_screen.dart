f6c589d47e68b0274af100e184f3b26173ea3ae7
AlphaXen <kimminjae771@naver.com>
2025-09-28 01:49:54 +0900

    feat(home_screen): Initialize WebViewController

--- lib/week03/blog_web_app/screen/home_screen.dart
+++ lib/week03/blog_web_app/screen/home_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class HomeScreen extends
+class HomeScreen extends StatelessWidget {
+
+  // WebViewController 선언
+  WebViewController webViewController = WebViewController();
+
+  
+}
