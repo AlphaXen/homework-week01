b1ad8146d43ec0fad383954591ff545a6554c46c
AlphaXen <kimminjae771@naver.com>
2025-09-28 01:52:53 +0900

    feat: Add AppBar to HomeScreen with orange background

--- lib/week03/blog_web_app/screen/home_screen.dart
+++ lib/week03/blog_web_app/screen/home_screen.dart
@@ -11,7 +11,12 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      // 앱바 위젯 추가
+      appBar: AppBar(
+        // 배경색 지정
+        backgroundColor: Colors.orange,
+
+      ),
     )
   }
 }
