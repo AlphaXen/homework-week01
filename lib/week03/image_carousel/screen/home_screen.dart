d09ca3989fd58fa8aed6f8fba53abd26cad60e90
AlphaXen <kimminjae771@naver.com>
2025-09-28 02:25:38 +0900

    Updated Changes

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -9,12 +9,12 @@ class HomeScreen extends StatelessWidget {
     // 상태바 색상 변경
     // 상태바가 이미 흰색이면 light 대신 dark를 주어 검정으로 바꿔주세요.
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
-    
+
     return Scaffold(
       body: PageView( // PageView 추가
         children: [1, 2, 3, 4, 5] // 샘플 리스트 생성
           .map( // 위젯으로 매핑
-            (number) => Image.asset('asset/img/image_$number.jpeg'),
+            (number) => Image.asset('asset/img/image_$number.jpeg',
             fit: BoxFit.cover, // BoxFit.cover 설정
           )
           .toList(),
