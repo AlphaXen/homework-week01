f0ae97677959318ef1ffc33514519c076ee0c4e3
AlphaXen <kimminjae771@naver.com>
2025-09-28 23:16:11 +0900

    Fix: Remove unused setState in onHeartPressed

--- lib/week04/u_and_i/screen/home_screen.dart
+++ lib/week04/u_and_i/screen/home_screen.dart
@@ -36,9 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
 void onHeartPressed() { // 함수 눌렀을 때 실행할 함수
-  setState(() {
-    firstDay = firstDay.subract(Duration(days: 1));
-  });
+  
 }
 }
 
