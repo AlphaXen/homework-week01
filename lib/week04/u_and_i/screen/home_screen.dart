837ffbbcb0bdc3c1fb7d204d919ba6ea80d11301
AlphaXen <kimminjae771@naver.com>
2025-09-28 23:15:10 +0900

    Fix: Corrected typo in `subract` to `subtract`

--- lib/week04/u_and_i/screen/home_screen.dart
+++ lib/week04/u_and_i/screen/home_screen.dart
@@ -34,12 +34,12 @@ class _HomeScreenState extends State<HomeScreen> {
      ),
     );
   }
-}
 
 void onHeartPressed() { // 함수 눌렀을 때 실행할 함수
   setState(() {
     firstDay = firstDay.subract(Duration(days: 1));
   });
+}
 }
 
 class _DDay extends StatelessWidget {
