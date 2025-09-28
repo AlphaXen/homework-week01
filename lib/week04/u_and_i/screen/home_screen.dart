ee493c84de3a0c575ed37160950e7f8350f8aa5c
AlphaXen <kimminjae771@naver.com>
2025-09-28 22:59:10 +0900

    feat(home_screen): Pass onHeartPressed callback to _DDay widget

--- lib/week04/u_and_i/screen/home_screen.dart
+++ lib/week04/u_and_i/screen/home_screen.dart
@@ -23,6 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           _DDay(
+            // 하트 눌렀을 때 실행할 함수 전달하기
             onHeartPressed: onHeartPressed,
           ),
           _CoupleImage(),
