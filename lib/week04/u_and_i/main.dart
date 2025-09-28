9e2e5b68ae4a773499d7570729b564640b66b353
AlphaXen <kimminjae771@naver.com>
2025-09-28 22:47:40 +0900

    feat(u_and_i): Add comments explaining theme customization

--- lib/week04/u_and_i/main.dart
+++ lib/week04/u_and_i/main.dart
@@ -4,14 +4,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'sunflower',
-        textTheme: TextTheme(
-          headlineLarge: TextStyle(
-            color: Colors.white,
-            fontSize: 80.0,
-            fontWeight: FontWeight.w700,
-            fontFamily: 'parisienne',
+      theme: ThemeData( // 테마를 지정할 수 있는 클래스
+        fontFamily: 'sunflower', // 기본 글씨체
+        textTheme: TextTheme( // 글자 테마를 적용할 수 있는 클래스
+          headlineLarge: TextStyle( // headline1 스타일 정의
+            color: Colors.white, // 글 색상
+            fontSize: 80.0, // 크기
+            fontWeight: FontWeight.w700, // 글 두께
+            fontFamily: 'parisienne', // 글씨체
           ),
           headlineMedium: TextStyle(
             color: Colors.white,
